b4d37e29dfd2f1e6e3f12b4d698584effa220fff
vansh272 <shahvansh272@gmail.com>
2025-08-12 22:59:53 +0530

    Create netsalary.c

---
+++ netsalary.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    float gross, allowance, deduction, net;
+
+    printf("Enter gross salary: ");
+    scanf("%f", &gross);
+
+    if (gross > 10000) {
+        allowance = 0.10 * gross;
+        deduction = 0.03 * gross;
+    } else if (gross > 5000) {
+        allowance = 0.07 * gross;
+        deduction = 0.02 * gross;
+    } else {
+        allowance = 0;
+        deduction = 0;
+    }
+
+    net = gross + allowance - deduction;
+
+    printf("Net salary = %f\n", net);
+
+    return 0;
+}
